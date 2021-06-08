afc15961640a2fc740e02c5b86f2dc686b86ef18
RomainLPromoLyraJS <romain75l@gmail.com>
2021-06-08 20:09:47 +0200

    Darkmode started / setting up projects page with content and CSS / assets file added

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 const Header = () => {
+    
+    const [isDarkMode, setDarkMode] = React.useState(false);
+    const toggleDarkMode = (checked: boolean) => {
+      setDarkMode(checked);
+    };
+
     return (
+        
         <header className="header">
             <div className="header__container">
                 <NavLink to="/" className="header__container__link">
@@ -13,7 +21,9 @@ const Header = () => {
                 <NavLink to="/about" className="header__links__link">Qui-suis-je ?</NavLink>
                 <NavLink to="/resume" className="header__links__link">Mon CV</NavLink>
                 <NavLink to="/projects" className="header__links__link">Mes projets</NavLink>
+                <DarkModeSwitch className="header__links__darkmode header__links__darkmode--active" checked={isDarkMode} onChange={toggleDarkMode} size={50}/>
             </div>
+       
         </header>
     );
 };
--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,9 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+
+//Local imports
+
+import CrepeImg from '../assets/img/cameron-venti-MPAkVKZgqak-unsplash.jpg';
 
 const Projects = () => {
     return (
         <div className="projects">
-            <p>je suis dans mes projets.</p>
+            <h1 className="projects__title">Mes projets</h1>
+            <h2 className="projects__subtitle">Réalisés avec ReactJS</h2>
+            <div className="projects__section">
+                <div className="projects__section__project">
+                    <img className="projects__section__project__img" src={CrepeImg} alt="site d'une recette de crêpes en ReactJS" /> 
+                </div>
+                <div className="projects__section__project__links">
+                    <a className="projects__section__project__links__link" href="#" target="_blank">recette de crêpes</a>
+                </div>
+            </div>
+            <div className="projects__section">
+                <div className="project__section__project">
+                    <img className="projects__section__project__img" src={CrepeImg} alt="site d'une recette de crêpes en ReactJS" /> 
+                </div>
+                <div className="projects__section__project__links">
+                    <a className="projects__section__project__links__link" href="#" target="_blank">recette de crêpes</a>
+                </div>
+            </div>
         </div>
     );
 };
